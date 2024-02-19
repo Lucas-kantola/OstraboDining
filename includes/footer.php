14363f0472e204d5743bae12fd679afe7c45fa5c
Lucas-kantola <114872825+Lucas-kantola@users.noreply.github.com>
2024-02-19 23:10:57 +0100

    Main done

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,21 @@
-		
+<div class="footer">
+    <div class="flex">
+        <div class="column">
+            <h3>Phone</h3>
+            <p>0522 - 69 74 20</p>
+        </div>
+        <div class="column">
+            <h3>Vi finns</h3>
+            <p>kämpegatan 36, 451 32 <br>Uddevalla</p>
+        </div>
+        <div class="column">
+            <h3>Öppetider</h3>
+            <p><i>Tisdag - Torsdag</i><br>1.00pm - 9.00pm</p>
+            <p><i>Fredag - Lördag</i><br>4.00pm - 11.00pm</p>
+            <p><i>Söndag - Måndag</i><br>Closed</p>
+        </div>
+    </div>
+    <div class="copyright">
+        &copy; <?php echo date("Y"); ?> <a href="index.php">Östrabo1 Dining</a>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 	define("TITLE", "Home | Östrabo1 Dining");
+	$content = file_get_contents('http://loripsum.net/api');
 ?>
-
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php echo TITLE ?></title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<?php include('includes/header.php'); ?>
+		<h1>Östrabo1 Dining</h1>
+		<h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere aut a asperiores, cum nobis excepturi inventore, incidunt necessitatibus explicabo ad voluptatem voluptas, iste vel unde. Iusto sequi ipsam voluptates aut!</h4>
+		<h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed distinctio non alias unde quos dolorem fuga animi magni ea? Vero expedita voluptatum blanditiis atque totam!</h4>
+		<?php include('includes/footer.php'); ?>
+	</div>
+</body>
+</html>
